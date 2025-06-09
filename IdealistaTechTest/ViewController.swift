8fb31e3321e7b2bcffec0ed121fc170ca88ccbca
Aitor Ballesteros <aitor.ballesteros@appspace.com>
2025-06-09 15:58:46 +0200

    Initial Commit

---
+++ IdealistaTechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdealistaTechTest
+//
+//  Created by APPSPACE on 9/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
